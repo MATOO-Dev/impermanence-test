fc267adde8457d400fb366067386deca8f8935c4
MATOO-Dev <matoo-dev@proton.me>
2026-05-16 11:28:46 +0200

    Imported template config

---
+++ configuration.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+{
+	imports = [
+		./hardware-configuration.nix
+	];
+
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	networking.hostName = "nixos";
+	networking.networkmanager.enable = true;
+
+	time.timeZone = "Europe/Berlin";
+	i18n.defaultLocale = "en_US.UTF-8";
+
+	users.users.matoo = {
+		isNormalUser = true;
+		initialPassword = "12345";
+		extraGroups = ["wheel"];
+		packages = with pkgs; [
+			tree
+		];
+	};
+
+	environment.systemPackages = with pkgs; [
+		vim
+		neovim
+		nh
+	];
+
+	services.openssh.enable = true;
+
+	system.stateVersion = "25.11";
+}
